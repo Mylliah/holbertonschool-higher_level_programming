b237c5d05e6ccb79c70f8a70e2d323c03818ab16
Mylliah <mezhoud.my@gmail.com>
2025-05-06 23:16:09 +0200

    Refactor print statements to use format method for consistency across scripts and update README with correct links for all tasks

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 100):
     if i < 99:
-        print(f"{i}, ", end="")
+        print("{:02d}, ".format(i), end="")
     else:
-        print(f"{i}")
+        print("{:02d}".format(i))
