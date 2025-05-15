cb0ca124344fca996ddba4f16c8e3e9db777cf32
Mylliah <mezhoud.my@gmail.com>
2025-05-15 15:52:48 +0200

    Refactor safe_print_list_integers to store value in a variable before printing

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            value = my_list[i]
+            print("{:d}".format(value), end="")
             count += 1
         except (ValueError, TypeError):
             continue
