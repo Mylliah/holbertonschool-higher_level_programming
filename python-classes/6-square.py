c60f5921e8762cecc0bb9355f43f06553a2b4267
Mylliah <mezhoud.my@gmail.com>
2025-05-20 14:34:23 +0200

    Add size validation in Square constructor to ensure proper initialization

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -15,6 +15,10 @@ class Square:
             size (int): size of the square must be an integer >= 0
             position (tuple): position must be a tuple of 2 positive integers
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         self.position = position
 
