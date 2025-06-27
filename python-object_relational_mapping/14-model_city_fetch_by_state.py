248899c764452c1433b875471025c5422132df42
Mylliah <mezhoud.my@gmail.com>
2025-06-27 22:07:02 +0200

    Fix formatting of docstring in city fetch script

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database hbtn_0e_14_usa
-in the format: <state name>: (<city id>) <city name>."""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+in the format: <state name>: (<city id>) <city name>.
+"""
 
 from sys import argv
 from sqlalchemy import create_engine
