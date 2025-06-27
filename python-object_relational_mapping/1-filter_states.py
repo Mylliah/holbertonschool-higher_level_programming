b1d1de84cd86850d5d5072341d5db1f98c6f67e1
Mylliah <mezhoud.my@gmail.com>
2025-06-27 20:32:02 +0200

    Fix shebang line and remove unused function definition in filter states script

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
 Uses MySQLdb to connect to MySQL and filter the results.
@@ -7,11 +7,7 @@ Uses MySQLdb to connect to MySQL and filter the results.
 import MySQLdb
 import sys
 
-
-def list_states_starting_with_N():
-    """
-    Connects to MySQL and prints states whose names start with 'N'.
-    """
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -37,7 +33,3 @@ def list_states_starting_with_N():
 
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    list_states_starting_with_N()
